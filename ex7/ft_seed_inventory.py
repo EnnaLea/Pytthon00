60becb79a7451216f90c35856edc4f789882fbc9
EnnaLea <soleariaeve@gmail.com>
2026-08-03 19:42:15 +0200

    correzione identatura

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -6,13 +6,13 @@
 #    By: ealiman <ealiman@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/08/03 19:01:43 by ealiman           #+#    #+#              #
-#    Updated: 2026/08/03 19:25:07 by ealiman          ###   ########.fr        #
+#    Updated: 2026/08/03 19:41:48 by ealiman          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     if unit == 'packets':
-       print(f"{seed_type.capitalize()} seed: {quantity} packets availble")
+        print(f"{seed_type.capitalize()} seed: {quantity} packets availble")
     elif unit == 'grams':
         print(f"{seed_type.capitalize()} seed: {quantity} grams total")
     elif unit == 'area':
